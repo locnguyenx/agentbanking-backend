3696ad3543bc868a68105908850fa11b0b27f3f1
locnguyenx <loc.nguyenxuan@gmail.com>
2026-04-05 09:15:15 +0700

    feat: register proxy enquiry use case bean (Law V compliance)

--- services/switch-adapter-service/src/main/java/com/agentbanking/switchadapter/config/DomainServiceConfig.java
+++ services/switch-adapter-service/src/main/java/com/agentbanking/switchadapter/config/DomainServiceConfig.java
@@ -1,7 +1,9 @@
 package com.agentbanking.switchadapter.config;
 
 import com.agentbanking.switchadapter.application.usecase.TransactionQuoteUseCaseImpl;
+import com.agentbanking.switchadapter.application.usecase.ProxyEnquiryUseCaseImpl;
 import com.agentbanking.switchadapter.domain.port.out.FeeCalculationGateway;
+import com.agentbanking.switchadapter.domain.port.out.DuitNowProxyGateway;
 import com.agentbanking.switchadapter.domain.service.SwitchAdapterService;
 import com.agentbanking.switchadapter.domain.port.out.SwitchTransactionRepository;
 import org.springframework.context.annotation.Bean;
@@ -18,5 +20,10 @@ public class DomainServiceConfig {
     @Bean
     public TransactionQuoteUseCaseImpl transactionQuoteUseCase(FeeCalculationGateway feeCalculationGateway) {
         return new TransactionQuoteUseCaseImpl(feeCalculationGateway);
+    }
+
+    @Bean
+    public ProxyEnquiryUseCaseImpl proxyEnquiryUseCase(DuitNowProxyGateway duitNowProxyGateway) {
+        return new ProxyEnquiryUseCaseImpl(duitNowProxyGateway);
     }
 }
