6f64329d8103bc8d4114736f0df40afddde26035
locnguyenx <loc.nguyenxuan@gmail.com>
2026-04-05 14:46:28 +0700

    feat: add AUDIT_LOGS topic to KafkaTopics registry

--- common/src/main/java/com/agentbanking/common/messaging/KafkaTopics.java
+++ common/src/main/java/com/agentbanking/common/messaging/KafkaTopics.java
@@ -7,6 +7,7 @@ public final class KafkaTopics {
     public static final String SMS_NOTIFICATIONS = "sms.notifications";
     public static final String EFM_EVENTS = "efm.events";
     public static final String COMMISSION_EVENTS = "commission.events";
+    public static final String AUDIT_LOGS = "audit-logs";
 
     private KafkaTopics() {
     }
