1e4f7c5582ce15b9339ccaf879f5f99f1664d8e7
locnguyenx <loc.nguyenxuan@gmail.com>
2026-04-05 09:13:46 +0700

    feat: implement proxy enquiry use case (FR-002)

---
+++ services/switch-adapter-service/src/main/java/com/agentbanking/switchadapter/application/usecase/ProxyEnquiryUseCaseImpl.java
@@ -0,0 +1,26 @@
+package com.agentbanking.switchadapter.application.usecase;
+
+import com.agentbanking.common.security.ErrorCodes;
+import com.agentbanking.switchadapter.domain.port.in.ProxyEnquiryUseCase;
+import com.agentbanking.switchadapter.domain.port.out.DuitNowProxyGateway;
+
+public class ProxyEnquiryUseCaseImpl implements ProxyEnquiryUseCase {
+
+    private final DuitNowProxyGateway duitNowProxyGateway;
+
+    public ProxyEnquiryUseCaseImpl(DuitNowProxyGateway duitNowProxyGateway) {
+        this.duitNowProxyGateway = duitNowProxyGateway;
+    }
+
+    @Override
+    public ProxyEnquiryResult enquiryProxy(String proxyId, String proxyType) {
+        try {
+            String name = duitNowProxyGateway.resolveProxy(proxyId, proxyType);
+            return new ProxyEnquiryResult(name, proxyType);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new IllegalStateException(ErrorCodes.ERR_EXT_PROXY_ENQUIRY_FAILED + ": " + e.getMessage(), e);
+        }
+    }
+}
